d01220a419277a63151b64608b4fe6179718340b
HigorLucas <higor.freitas@neppoadm.com.br>
2019-09-17 22:39:45 -0300

    Inicio de projeto

---
+++ src/main/java/com/ProjetoNovo/DetonandoJava/DetonandoJavaApplication.java
@@ -0,0 +1,13 @@
+package com.ProjetoNovo.DetonandoJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DetonandoJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DetonandoJavaApplication.class, args);
+	}
+
+}
